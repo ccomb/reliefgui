b1db3fd7916f1878630e8024026a6766f30266db
Christophe Combelles <ccomb@free.fr>
2009-09-29 23:57:59 +0200

    Improved form handling and design

--- reliefgui/controllers/index.py
+++ reliefgui/controllers/index.py
@@ -6,44 +6,47 @@ from pylons.decorators import validate
 
 from reliefgui.lib.base import BaseController, render
 from reliefcnc.shoot import ReliefShooter
-import formencode
+from formencode import htmlfill, Schema, validators
 
 log = logging.getLogger(__name__)
 
-class ShootForm(formencode.Schema):
-    base = formencode.validators.Int(not_empty=True)
-    mode = formencode.validators.OneOf(['slow', 'burst'])
+class ShootForm(Schema):
+    base = validators.Int(not_empty=True)
+    mode = validators.OneOf(['slow', 'burst', 'manual'])
 
 class IndexController(BaseController):
 
     def index(self):
-        # Return a rendered template
-        #return render('/index.mako')
-        # or, return a response
+        errors = None
+        form_result = None
+        c.nb_points = 8
+        if request.POST:
+            schema = ShootForm()
+            try:
+                form_result = schema.to_python(request.POST)
+            except validators.Invalid, error:
+                errors = error.unpack_errors()
+            else:
+                base = form_result['base']
+                mode = form_result['mode']
+
+                shooter = ReliefShooter()
+                shooter.resolution = 100.0/3
+                shooter.maxrange = 360
+                shooter.nb_points = c.nb_points
+                shooter.base = base
+                if mode == 'slow':
+                    shooter.slow()
+                elif mode == 'burst':
+                    shooter.burst()
+                elif mode == 'manual':
+                    shooter.manual()
+                shooter.off()
+                del shooter
+
+        c.shootform = htmlfill.render(render('/shootform.mako'),
+                                         defaults=request.POST,
+                                         errors=errors)
+
         return render('index.pt')
-
-    def form(self):
-        return render('form.mako')
-
-    @validate(schema=ShootForm(), form='index')
-    def capture(self):
-        base = self.form_result['base']
-        mode = self.form_result['mode']
-
-        shooter = ReliefShooter()
-        shooter.resolution = 100.0/3
-        shooter.maxrange = 360
-        shooter.nb_points = 3
-        shooter.base = base
-        if mode == 'slow':
-            shooter.slow()
-        elif mode == 'burst':
-            shooter.burst()
-
-        shooter.off()
-        del shooter
-
-
-        redirect_to(action='index')
-
 
